9e4c5282671b672bcdf75c3e2ddb8c4264e6f275
Aytug <aaytugozkaya@gmail.com>
2022-10-10 13:12:03 +0300

    .Gitignore ve .gitattributes ekle.

---
+++ Hafta2/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Hafta2.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hafta2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public string[] Kayit()
+        {
+            string[] ogr = new string[3];
+            return ogr;
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Hafta2/Controllers/OgrenciController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hafta2.Controllers
+{
+    public class OgrenciController : Controller
+    {
+        public IActionResult OgrEkle()
+        {
+            return View();
+        }
+        public IActionResult OgrList()
+        {
+            return View();
+        }
+        [HttpPost]
+        public string OgrKaydet(string ogrAd, string ogrSoyad, string ogrNo)
+        {
+            string message=ogrAd + " " + ogrSoyad + " " + ogrNo + " kaydedildi." ;
+            return message;
+        }
+    }
+}
